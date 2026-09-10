5b39ea83c6db556a0906b999f5de6e5c4645f662
Christian Gulliksen <christian@kjempehjerne.no>
2026-09-10 14:06:34 +0200

    feat: Exercise 5A - COMPONENT_MAO - lazy loading mdx components

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,9 +4,8 @@ import BlogHero from '@/components/BlogHero';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { BLOG_TITLE } from '@/constants';
-import CodeSnippet from '@/components/CodeSnippet';
-
 import styles from './postSlug.module.css';
+import COMPONENT_MAP from '@/helpers/mdx-components';
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -31,9 +30,7 @@ async function BlogPost({ params }) {
       <div className={styles.page}>
         <MDXRemote
           source={content}
-          components={{
-            pre: CodeSnippet,
-          }}
+          components={COMPONENT_MAP}
         />
       </div>
     </article>
