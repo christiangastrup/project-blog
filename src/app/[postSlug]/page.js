68e845f05f2fabc438bde96180ac1375234622e7
Christian Gulliksen <christian@kjempehjerne.no>
2026-09-11 14:57:08 +0200

    feat: stretch 2 - 404

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import BlogHero from '@/components/BlogHero';
-import { loadBlogPost } from '@/helpers/file-helpers';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import { BLOG_TITLE } from '@/constants';
-import styles from './postSlug.module.css';
-import COMPONENT_MAP from '@/helpers/mdx-components';
+import BlogHero from "@/components/BlogHero";
+import { loadBlogPost } from "@/helpers/file-helpers";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { BLOG_TITLE } from "@/constants";
+import styles from "./postSlug.module.css";
+import COMPONENT_MAP from "@/helpers/mdx-components";
+
+import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
-  const { frontmatter } = await loadBlogPost(postSlug);
+  const blogpostData = await loadBlogPost(postSlug);
+
+  if (!blogpostData) {
+    return null;
+  }
+
+  const { frontmatter } = blogpostData;
 
   return {
     title: `${frontmatter.title} • ${BLOG_TITLE}`,
@@ -19,7 +27,13 @@ export async function generateMetadata({ params }) {
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
-  const { frontmatter, content } = await loadBlogPost(postSlug);
+  const blogpostData = await loadBlogPost(postSlug);
+
+  if (!blogpostData) {
+    notFound();
+  }
+
+  const { frontmatter, content } = blogpostData;
 
   return (
     <article className={styles.wrapper}>
@@ -28,10 +42,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote
-          source={content}
-          components={COMPONENT_MAP}
-        />
+        <MDXRemote source={content} components={COMPONENT_MAP} />
       </div>
     </article>
   );
